736a67aa8985e780878c09f22fcb0fb8d7fa050a
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2015-04-29 10:26:18 -0400

    Fix the AccountNotFoundException to include accountId information. GREEN

--- src/main/java/example/banking/services/AccountNotFoundException.java
+++ src/main/java/example/banking/services/AccountNotFoundException.java
@@ -2,10 +2,17 @@ package example.banking.services;
 
 public class AccountNotFoundException extends Exception {
 
-	private static final long serialVersionUID = 2826695155790834813L;
+	private static final long serialVersionUID = -1629232289067405862L;
+
+	private int accountId;
 
 	public AccountNotFoundException(int accountId) {
 		super(String.format("Account #%d was not found", accountId));
+		this.accountId = accountId;
+	}
+
+	public int getAccountId() {
+		return accountId;
 	}
 
 }
