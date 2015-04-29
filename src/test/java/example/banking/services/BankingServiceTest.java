afa3bc75f9ded4437dda7326acef2c9e5240468f
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2015-04-29 10:24:14 -0400

    Alter existing tests, add verification for non-existing account id. RED

--- src/test/java/example/banking/services/BankingServiceTest.java
+++ src/test/java/example/banking/services/BankingServiceTest.java
@@ -77,6 +77,7 @@ public class BankingServiceTest {
 			Assert.assertNotNull(e);
 			String expected = "Account #" + fromAccountId + " was not found";
 			Assert.assertEquals(expected, e.getMessage());
+			Assert.assertEquals(fromAccountId, e.getAccountId());
 		}
 	}
 
@@ -97,6 +98,7 @@ public class BankingServiceTest {
 			Assert.assertNotNull(e);
 			String expected = "Account #" + toAccountId + " was not found";
 			Assert.assertEquals(expected, e.getMessage());
+			Assert.assertEquals(toAccountId, e.getAccountId());
 		}
 	}
 
