f67bf2aa7aea40c90c026279a4e29d8c243a92b9
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2015-04-29 07:20:39 -0400

    Implement InMemoryAccountDao to autogenerate ids. GREEN

--- src/main/java/example/banking/dao/AccountDao.java
+++ src/main/java/example/banking/dao/AccountDao.java
@@ -3,6 +3,6 @@ package example.banking.dao;
 import example.banking.domain.Account;
 
 public interface AccountDao {
-	Account create(int id, String owner, double balance);
+	Account create(String owner, double balance);
 	Account find(int id);
 }
--- src/main/java/example/banking/dao/InMemoryAccountDao.java
+++ src/main/java/example/banking/dao/InMemoryAccountDao.java
@@ -8,9 +8,11 @@ import example.banking.domain.Account;
 public class InMemoryAccountDao implements AccountDao {
 
 	private static Map<Integer,Account> database = new HashMap<>();
+	private int counter = 1;
 	
 	@Override
-	public synchronized Account create(int id, String owner, double balance) {
+	public synchronized Account create(String owner, double balance) {
+		int id = counter++;
 		Account account = new Account(id,owner,balance);
 		database.put(Integer.valueOf(id), account);
 		return account;
