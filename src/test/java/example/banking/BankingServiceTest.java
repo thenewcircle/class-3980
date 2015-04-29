84ddea6e44540961bee9ac93056277c7463445d3
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2015-04-29 05:01:08 -0400

    Add test structure: Assemble, Test Fixture, Act, Verify, Cleanup. RED

--- src/test/java/example/banking/BankingServiceTest.java
+++ src/test/java/example/banking/BankingServiceTest.java
@@ -13,13 +13,27 @@ public class BankingServiceTest {
 	@Test
 	public void testTransfer() {
 
+		// assemble
+		AccountDao dao = new InMemoryAccountDao();
 		BankingService teller = new SimpleBankingService();
 
-		int fromAccountId = 1;
-		int toAccountId = 2;
+		// test fixtures
+		Account fromAccount = dao.create(1, 10_000_000.00);
+		Account toAccount = dao.create(2, 5.00);
+		int fromAccountId = fromAccount.getId();
+		int toAccountId = toAccount.getId();
 		double amount = 1_000_000.00;
 
+		// act
 		teller.transfer(fromAccountId, toAccountId, amount);
+
+		// verify
+		Account finalFromAccount = dao.find(fromAccountId);
+		Account finalToAccount = dao.find(toAccountId);
+		Assert.assertEquals(10_000_000.0 - 1_000_000.0, finalFromAccount.getBalance());
+		Assert.assertEquals(5.0 + 1_000_000.0, finalToAccount.getBalance());
+
+		// cleanup
 
 	}
 
