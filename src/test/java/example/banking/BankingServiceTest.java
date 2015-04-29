ff5ddd6cb1f231dfc6eb75afe652a94f625c6cd3
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2015-04-29 04:26:03 -0400

    Added first test.

---
+++ src/test/java/example/banking/BankingServiceTest.java
@@ -0,0 +1,13 @@
+package example.banking;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankingServiceTest {
+
+	@Test
+	public void testHelloWorld() {
+		Assert.assertEquals(1, 1);
+	}
+
+}
